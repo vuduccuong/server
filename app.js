57f04f62d2086e1b698e716dff0818aaf0e37213
Cường VĐ <vuduccuong.ck48@gmail.com>
2020-11-18 20:02:05 +0700

    crors

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const path = require('path');
-
-const publicPath = path.join(__dirname, '..', 'public');
 
 const PORT = process.env.PORT || 5000;
 const {MONGOURI} = require('./keys');
@@ -19,14 +16,6 @@ const corsOptions = {
     methods: "GET, PUT,POST, PATCH"
 }
 
-app.use(cors(corsOptions));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  };
-
-app.use(express.static(publicPath));
 //connect to db
 mongoose.connect(MONGOURI,{
     
@@ -42,15 +31,11 @@ mongoose.connection.on('error', (err)=>{
     console.log("connect fail", err);
 });
 
-
+app.use(cors());
 //router
 app.use(express.json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
 
 app.listen(PORT, () => {
     console.log('app running on ', PORT);
